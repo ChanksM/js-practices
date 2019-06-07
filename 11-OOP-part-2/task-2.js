909024cada76115d2f50eaab089530a8c7d7e848
ChanksM <mushni.chankseliani@geolab.edu.ge>
2019-06-07 20:21:47 +0400

    feat(11-oop-part-2): Finish all tasks

---
+++ 11-OOP-part-2/task-2.js
@@ -0,0 +1,23 @@
+function CoffeeMachine(power) {
+    this.power = power;
+    this.waterAmount = 0;
+    this.WATER_HEAT_CAPACITY = 4200;
+}
+
+CoffeeMachine.prototype.getTimeToBoil = function() {
+    return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power;
+}
+
+CoffeeMachine.prototype.run = function() {
+    setTimeout(function() {
+        console.log('Coffee is ready!');
+    }, this.getTimeToBoil());
+}
+
+CoffeeMachine.prototype.setWaterAmount = function(amount) {
+    this.waterAmount = amount;
+}
+
+let coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.setWaterAmount(50);
+coffeeMachine.run();
