9becc8eb841cbee1b5d07e7667aa3731b7c71273
ChanksM <mushni.chankseliani@geolab.edu.ge>
2019-06-13 20:41:06 +0400

    feat(17-promise): Finish task-1

---
+++ 17-promise/task-1.js
@@ -0,0 +1,26 @@
+function isCustomerVerified(item) {
+    return new Promise(function(resolve, reject) {
+        if(item.verified === true) {
+            resolve('true');
+        } else {
+            reject('Customer is not verified');
+        }
+    })
+};
+
+const personFirst = {
+    name: 'Oliver',
+    verified: true
+};
+
+const personSecond = {
+    name: 'Alex'
+};
+
+isCustomerVerified(personFirst)
+    .then(status => console.log(status)) // true
+    .catch(error => console.log(error))
+    
+isCustomerVerified(personSecond)
+    .then(status => console.log(status))
+    .catch(error => console.log(error)) // Customer is not verified
